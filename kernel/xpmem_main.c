8ca5aed0b84f8bbe00e78109dfacb52ee554dc85
Kazuya Okubo <kazuya.okubo@hpe.com>
2026-08-10 23:44:33 +0000

    Add Debian and Ubuntu DKMS build support
    kallsyms_lookup_name is no longer exported to kernel modules as of
    Linux Kernel version 5.7.0.  For RHEL, xpmem_kln_ptr was setup by
    parsing /proc/kallsyms to enable kallsyms_lookup_name.  Since this
    needs to be done for all Kernel version 5.7.0 or newer, setting up
    kallsyms_lookup_name is updated to use kprobe to dynamically acquire
    the kernel symbol.

--- kernel/xpmem_main.c
+++ kernel/xpmem_main.c
@@ -41,6 +41,10 @@
 #include <linux/sched/task.h>
 #endif
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
+#include <linux/kprobes.h>
+#endif
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,10,0)
 #define proc_set_user(_pde, _uid, _gid)				\
 	do {							\
@@ -408,17 +412,8 @@ static struct miscdevice xpmem_dev_handle = {
 	.fops = &xpmem_fops
 };
 
-/*
- * kallsyms_lookup_name() is not exported on RHEL,so the address
- * is parsed from /proc/kallsyms and passed as module argument
- * using xpmem_kln_arg while loading the module.
- */
-
-unsigned long xpmem_kln_arg = 0;
-
-#if (defined(RHEL_MAJOR) && RHEL_MAJOR == 9 && RHEL_MINOR >= 3)
-void* (*xpmem_kln_ptr)(char*) ;
-EXPORT_SYMBOL(xpmem_kln_ptr);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
+typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
 #endif
 
 pte_t * (*p_huge_pte_offset) (struct mm_struct *mm, unsigned long addr, unsigned long sz);
@@ -433,14 +428,19 @@ xpmem_init(void)
 	struct proc_dir_entry *global_pages_entry;
 	struct proc_dir_entry *debug_printk_entry;
 
-#if (defined(RHEL_MAJOR) && RHEL_MAJOR == 9 && RHEL_MINOR >= 3)
-        xpmem_kln_ptr = ( void*(*)(char*) ) xpmem_kln_arg;
-
-        if (xpmem_kln_ptr == NULL) {
-                printk(KERN_ERR
-                        "XPMEM: xpmem module xpmem_kln_arg module_param missing\n");
-                return -EFAULT;
-        }
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 7, 0)
+	static struct kprobe kp = {
+		.symbol_name = "kallsyms_lookup_name"
+	};
+	kallsyms_lookup_name_t kallsyms_lookup_name;
+	ret = register_kprobe(&kp);
+	if (ret < 0) {
+		printk(KERN_ERR
+			"XPMEM: register_kprobe failed. ret=%d\n", ret);
+		return ret;
+	}
+	kallsyms_lookup_name = (kallsyms_lookup_name_t)kp.addr;
+	unregister_kprobe(&kp);
 #endif
 
         p_huge_pte_offset = (void *)kallsyms_lookup_name("huge_pte_offset");
@@ -535,7 +535,5 @@ MODULE_AUTHOR("Hewlett Packard Enterprise");
 MODULE_INFO(supported, "external");
 MODULE_DESCRIPTION("XPMEM support");
 MODULE_VERSION(XPMEM_CURRENT_VERSION_STRING);
-module_param(xpmem_kln_arg, ulong, 0);
-MODULE_PARM_DESC(xpmem_kln_arg, "kallsyms_lookup_name address");
 module_init(xpmem_init);
 module_exit(xpmem_exit);
